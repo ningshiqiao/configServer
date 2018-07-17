79c6963ad81623593a057c0e126393959b880d72
宁石乔 <ningsq@mfhcd.com>
2018-07-17 22:31:44 +0800

    config

--- src/main/java/com/nsq/ConfigApplication.java
+++ src/main/java/com/nsq/ConfigApplication.java
@@ -1,8 +1,7 @@
-package com.ly;
+package com.nsq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
